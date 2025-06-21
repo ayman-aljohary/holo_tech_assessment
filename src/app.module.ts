c55234ac0e95ceaac7e597509513056a211d8e7e
Ayman Aljohary <aymanaljohary@Aymans-MacBook-Pro.local>
2025-06-21 05:15:41 +0400

    Finished

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { ThrottlerModule } from '@nestjs/throttler';
   imports: [
     SequelizeModule.forRoot({
       dialect: 'mysql',
-      host: 'host.docker.internal',
+      host: 'localhost',
       port: 3306,
       username: 'root',
       password: 'root',
